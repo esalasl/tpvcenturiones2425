0b0c33cdc604f51e58344692a1146e8578da8a24
Nowels033 <noeldomse@gmail.com>
2024-12-16 12:41:23 +0100

    uso de ContextMesas.js en pantallaCobrar OJO PANTALLA COMPROBAR NO FUNCIONA PORQUE USA OTRO CONTEXTO HABRIA QUE REFACTORIZAR Y USAR EL MISMO CONTEXTO EN TODA LA APP

--- App.js
+++ App.js
@@ -2,56 +2,53 @@ import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import PantallaInicial from "./components/PantallaInicial";
 import PantallaMesa from "./components/PantallaMesa";
-import PantallaMenuTipo from "./components/PantallaTipoDeMenu";
-import PantallaComanda from "./components/PantallaComanda";
+import PantallaCobrar  from "./components/PantallaCobrar";
 import mesasRefact from "./components/mesasRefact";
-import { AppProvider } from "./components/Context";
+import PantallaComanda from "./components/PantallaComanda";
+import PantallaMenuTipo from "./components/PantallaTipoDeMenu";
 import PantallaComprobar from "./components/PantallaComprobar";
-import PantallaCobrar from "./components/PantallaCobrar";
+import { MesasProvider } from "./components/MesasContext";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <AppProvider>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="mesasRefact">
-        <Stack.Screen
-          name="mesasRefact"
-          component={mesasRefact}
-          options={{ title: "Mesas " }}
-        />
-        {/* <Stack.Screen name="PantallaInicial" component={PantallaInicial} options={{ title: 'Mesas ' }} /> */}
-        <Stack.Screen
-          name="PantallaMesa"
-          component={PantallaMesa}
-          options={{ title: "Detalle de la Mesa" }}
-        />
-        <Stack.Screen
-          name="PantallaMenuTipo"
-          component={PantallaMenuTipo}
-          options={{ title: "MENÚS" }}
-        />
-        <Stack.Screen
-          name="PantallaComanda"
-          component={PantallaComanda}
-          options={{ title: "Comanda" }}
-        />
-        <Stack.Screen
-          name="PantallaComprobrar"
-          component={PantallaComprobar}
-          options={{ title: "Comprobar" }}
-        />
-      <Stack.Screen
-          name="PantallaCobrar"
-          component={PantallaCobrar}
-          options={{ title: "Cobrar" }}
-        />
-
-      </Stack.Navigator>
-    </NavigationContainer>
-    </AppProvider>
+    <MesasProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="mesasRefact">
+          <Stack.Screen
+            name="mesasRefact"
+            component={mesasRefact}
+            options={{ title: "Mesas" }}
+          />
+          <Stack.Screen
+            name="PantallaMesa"
+            component={PantallaMesa}
+            options={{ title: "Detalle de la Mesa" }}
+          />
+          <Stack.Screen
+            name="PantallaMenuTipo"
+            component={PantallaMenuTipo}
+            options={{ title: "MENÚS" }}
+          />
+          <Stack.Screen
+            name="PantallaComanda"
+            component={PantallaComanda}
+            options={{ title: "Comanda" }}
+          />
+          <Stack.Screen
+            name="PantallaComprobar"
+            component={PantallaComprobar}
+            options={{ title: "Comprobar" }}
+          />
+          <Stack.Screen
+            name="PantallaCobrar"
+            component={PantallaCobrar}
+            options={{ title: "Cobrar" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MesasProvider>
   );
 }
